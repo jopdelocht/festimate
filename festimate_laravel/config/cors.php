5124cc7c35fc953340df06293eee394a872bd5de
jopdel <opdelochtjohn@gmail.com>
2024-03-21 16:40:12 +0100

    BACKEND - Laravel sanctum installation

--- festimate_laravel/config/cors.php
+++ festimate_laravel/config/cors.php
@@ -15,20 +15,13 @@ return [
     |
     */
 
-    'paths' => ['/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
-    'supports_credentials' => false,
+    'paths'  =>  ['/*'],
+    'allowed_methods'  =>  ['*'],
+    'allowed_origins'  =>  ['*'],
+    'allowed_origins_patterns'  =>  [],
+    'allowed_headers'  =>  ['*'],
+    'exposed_headers'  =>  false,
+    'max_age'  =>  false,
+    'supports_credentials'  =>  false,
 
 ];
