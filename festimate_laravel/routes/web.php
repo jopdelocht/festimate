30b44ee1a34f18e4942c3d2d761a31752cf40c13
jopdel <opdelochtjohn@gmail.com>
2024-03-21 08:59:39 +0100

    Artisan migrate - DB connection - .env file: key generation + credentials

--- festimate_laravel/routes/web.php
+++ festimate_laravel/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+// use App\Models\User;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/presents', function () {
+    $presents = DB::select('SELECT * FROM presents');
+    return response()->json($presents);
 });
